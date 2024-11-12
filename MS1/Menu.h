c1db38ae3e472a0a8148cd6fed06d4abd1e454a3
jasmin <165960583+jg-aro@users.noreply.github.com>
2024-11-12 13:53:19 -0500

    Update Menu.h

--- MS1/Menu.h
+++ MS1/Menu.h
@@ -2,12 +2,13 @@
 Final Project Milestone 1
 Module: Menu
 Filename: Menu.h
-Version 1.0
+Version 1.1
 Author	Jasmin Aro
 Revision History
 -----------------------------------------------------------
 Date		Reason
 2024/11/12  Correct output reached
+2024/11/12  Clean up
 -----------------------------------------------------------
 I have done most coding by myself, with use of ChatGPT for
 debugging inquiries, specifically regarding namespaces. I had trouble
@@ -20,31 +21,24 @@ std, so there needed to be manual accessing.
 #define SENECA_MENU_H
 #include <iostream>
 
-
 namespace seneca {
    class MenuItem {
 	   char* m_content = nullptr;
 	   size_t m_indent = 0;
 	   size_t m_indentSize = 0;
 	   int m_rowNumber = 0;
-
+	   void setEmpty();
+	   bool isValid()const;
    public:
 	   MenuItem();
-	   void setEmpty();
 	   MenuItem(const char* content, int indent, int indentSize, int rowNumber);
 	   ~MenuItem();
 	   MenuItem(const MenuItem&) = delete;
 	   MenuItem& operator=(MenuItem&) = delete;
-	   std::ostream& display(std::ostream& ostr) const;
-
 	   std::ostream& display() const;
-
-	   bool isValid()const;
-
+	   std::ostream& display(std::ostream& ostr) const;
 	   operator bool() const;
    };
-   
-
 }
 #endif // !SENECA_MENU_H
 
